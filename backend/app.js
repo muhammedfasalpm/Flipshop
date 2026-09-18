de46b41c9c5dd7a7b47ee9d615dbd66bdc795625
fasal pk <fasalpk13@gmail.com>
2026-09-18 09:26:24 +0530

    Fix Render port binding

--- backend/app.js
+++ backend/app.js
@@ -66,7 +66,7 @@ app.use("/api/categories", categoryRoutes);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
